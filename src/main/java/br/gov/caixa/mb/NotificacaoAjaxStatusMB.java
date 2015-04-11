ebe9d48e3b9132f6f793ed4c9a3ad8a2d6e4d9a1
Fernando Franzim <fernando.franzim@gmail.com>
2015-04-10 21:24:56 -0300

    Alterando classe

--- src/main/java/br/gov/caixa/mb/NotificacaoAjaxStatusMB.java
+++ src/main/java/br/gov/caixa/mb/NotificacaoAjaxStatusMB.java
@@ -14,7 +14,7 @@ import lombok.Data;
 @ViewScoped
 public class NotificacaoAjaxStatusMB implements Serializable {
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 12L;
 
 	private String login;
 	private String nome;
